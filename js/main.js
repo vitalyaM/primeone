0b066405a3dd5c46a873c3f58f23d7be8bf6ab93
Vitalii <vitaliysuhorukov@mail.ru>
2019-12-23 23:39:55 +0300

    d

--- js/main.js
+++ js/main.js
@@ -62,86 +62,6 @@ function adaptive_function() {
 }
 	adaptive_function();
 function map(n){
-	google.maps.Map.prototype.setCenterWithOffset= function(latlng, offsetX, offsetY) {
-		var map = this;
-		var ov = new google.maps.OverlayView(); 
-		ov.onAdd = function() { 
-			var proj = this.getProjection(); 
-			var aPoint = proj.fromLatLngToContainerPixel(latlng);
-			aPoint.x = aPoint.x+offsetX;
-			aPoint.y = aPoint.y+offsetY;
-			map.panTo(proj.fromContainerPixelToLatLng(aPoint));
-			//map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
-		}
-		ov.draw = function() {};
-		ov.setMap(this);
-	};
-	var markers = new Array();
-	var infowindow = new google.maps.InfoWindow({
-		//pixelOffset: new google.maps.Size(-230,250)
-	});
-	var locations = [
-		[new google.maps.LatLng(40.742918,-73.926063)],
-	]
-	var options = {
-		zoom: 15,
-		panControl:false,
-		mapTypeControl:false,
-		center: locations[0][0],
-		styles:[{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}],
-		scrollwheel:true,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}; 
-	var map = new google.maps.Map(document.getElementById('map'), options);
-	var icon={
-		url:'img/icons/map.svg',
-		scaledSize: new google.maps.Size(18, 45),
-		anchor: new google.maps.Point(9, 10)
-	}
-	for (var i = 0; i < locations.length; i++) {
-		var marker = new google.maps.Marker({
-			icon:icon,
-			position: locations[i][0],
-			map: map,
-		});
-		google.maps.event.addListener(marker, 'click', (function (marker, i) {
-			return function () {
-				for (var m = 0; m < markers.length; m++) {
-					markers[m].setIcon(icon);
-				}
-					var cnt=i+1;
-				infowindow.setContent($('.contacts-map-item_'+cnt).html());
-				infowindow.open(map, marker);
-				marker.setIcon(icon);
-				map.setCenterWithOffset(marker.getPosition(),0,0);
-				setTimeout(function(){
-					baloonstyle();
-				},10);
-			}
-		})(marker, i));
-		markers.push(marker);
-	}
-
-	if(n){
-			var nc=n-1;
-		setTimeout(function(){
-			google.maps.event.trigger(markers[nc], 'click');
-		},500);
-	}
-}
-function baloonstyle(){
-	$('.gm-style-iw').parent().addClass('baloon');
-	$('.gm-style-iw').prev().addClass('baloon-style');
-	$('.gm-style-iw').next().addClass('baloon-close');
-	$('.gm-style-iw').addClass('baloon-content');
-}
-if($("#map").length>0){
-	map(1);
-}
-
-
-/* YA
-function map(n){
 	ymaps.ready(init);
 	function init(){ 
 		// Создание карты.
@@ -151,13 +71,13 @@ function map(n){
 			// Чтобы не определять координаты центра карты вручную,
 			// воспользуйтесь инструментом Определение координат.
 			controls: [],
-			center: [43.585525,39.723062],
+			center: [40.742918,-73.926063],
 			// Уровень масштабирования. Допустимые значения:
 			// от 0 (весь мир) до 19.
-			zoom: 10
+			zoom: 15
 		});
 		
-		myPlacemar = new ymaps.Placemark([43.585525,39.723062],{
+		myPlacemar = new ymaps.Placemark([40.742918,-73.926063],{
 			id:'2'
 		},{
 			// Опции.
@@ -180,7 +100,7 @@ function map(n){
 		myMap.behaviors.disable('scrollZoom');
 	}
 }
-*/
+
 //FORMS
 function forms(){
 	//SELECT
